de6c464527593b7b943603a05060e517e3e63e2d
Rayane Charif Chefchaouni <rcharif@izar1.izar.cluster>
2025-04-21 01:56:23 +0200

    part 3 till decoding steps inference done

--- nanofm/modeling/transformer_layers.py
+++ nanofm/modeling/transformer_layers.py
@@ -97,7 +97,7 @@ class Attention(nn.Module):
 
         self.attn_out_proj = nn.Linear(dim, dim, bias=proj_bias)
 
-    def forward(self, x: torch.Tensor, : Optional[torch.Tensor] = None) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         B, L, D = x.shape # Batch size, sequence length, and dimension
 
         # TODO: Compute the keys K, queries Q, and values V from x. Each should be of shape [B num_heads L head_dim].
@@ -125,6 +125,8 @@ class Attention(nn.Module):
         # Output projection
         x = self.attn_out_proj(x)
         return x
+    
+    
 class CrossAttention(nn.Module):
     """
     Multi-head cross-attention module.
@@ -138,14 +140,15 @@ class CrossAttention(nn.Module):
     def __init__(self, dim: int, head_dim: int = 64, qkv_bias: bool = False, proj_bias: bool = False):
         super().__init__()
         self.num_heads = dim // head_dim
+        self.head_dim = head_dim
         self.scale = head_dim ** -0.5
 
         # TODO: Define here the linear layer producing Q from the input x
-        ???
+        self.q_proj = nn.Linear(dim, dim, bias=qkv_bias)
 
         # TODO: Define here the linear layers producing K, V from the context
         # Hint: Do you need to define two different projections, or can you use a single one for both?
-        ???
+        self.kv_proj = nn.Linear(dim, 2 * dim, bias=qkv_bias)
 
         self.attn_out_proj = nn.Linear(dim, dim, bias=proj_bias)
 
@@ -154,27 +157,31 @@ class CrossAttention(nn.Module):
         _, M, _ = context.shape # _, context sequence length (M), _
 
         # TODO: Compute the queries Q from x. It should be of shape [B num_heads N head_dim].
-        q = ???
+        q = self.q_proj(x)
+        q = rearrange(q, 'b n (h d) -> b h n d', h=self.num_heads, d=self.head_dim)
 
         # TODO: Compute the keys K and values V from the context. Each should be of shape [B num_heads M head_dim].
-        q = ???
+        kv = self.kv_proj(context)
+        kv = rearrange(kv, 'b m (two h d) -> two b h m d', two=2, h=self.num_heads, d=self.head_dim)
+        k, v = kv[0], kv[1]
 
         # TODO: Compute the attention matrix (pre softmax) and scale it by 1/sqrt(d_k). It should be of shape [B num_heads N M].
         # Hint: Use the already defined self.scale
-        attn = ???
+        attn = torch.matmul(q, k.transpose(-2, -1)) * self.scale
 
         if mask is not None:
             mask = rearrange(mask, "b n m -> b 1 n m") # Unsqueeze for multi-head attention
             # TODO: Apply the optional attention mask. Wherever the mask is False, replace the attention 
             # matrix value by negative infinity → zero attention weight after softmax.
-            attn = ???
+            attn = attn.masked_fill(~mask, float('-inf'))
 
         # TODO: Compute the softmax over the last dimension
-        attn = ???
+        attn = F.softmax(attn, dim=-1)
 
         # TODO: Weight the values V by the attention matrix and concatenate the different attention heads
         # Make sure to reshape the output to the original shape of x, i.e. [B N D]
-        x = ???
+        x = torch.matmul(attn, v)
+        x = rearrange(x, 'b h n d -> b n (h d)')
         
         # Output projection
         x = self.attn_out_proj(x)
@@ -227,16 +234,16 @@ class DecoderBlock(nn.Module):
     """
     def __init__(self, dim: int, head_dim: int = 64, mlp_ratio: float = 4., use_bias: bool = False):
         super().__init__()
-        self.norm1 = ??? # TODO (use the LayerNorm defined above)
-        self.query_norm = ??? # TODO (use the LayerNorm defined above)
-        self.context_norm = ??? # TODO (use the LayerNorm defined above)
-        self.norm2 = ??? # TODO (use the LayerNorm defined above)
+        self.norm1 = LayerNorm(dim, bias=use_bias)
+        self.query_norm = LayerNorm(dim, bias=use_bias)
+        self.context_norm = LayerNorm(dim, bias=use_bias)
+        self.norm2 = LayerNorm(dim, bias=use_bias)
 
-        self.self_attn = ??? # TODO Attention layer
-        self.cross_attn = ??? # TODO CrossAttention layer
+        self.self_attn = Attention(dim, head_dim=head_dim, qkv_bias=use_bias, proj_bias=use_bias) # TODO Attention layer
+        self.cross_attn = CrossAttention(dim, head_dim=head_dim, qkv_bias=use_bias, proj_bias=use_bias) # TODO CrossAttention layer
 
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = ??? # TODO MLP layer
+        self.mlp = Mlp(dim, hidden_features=mlp_hidden_dim, out_features=dim, bias=use_bias) # TODO MLP layer
 
     def forward(self, 
             x: torch.Tensor, 
@@ -250,8 +257,26 @@ class DecoderBlock(nn.Module):
         # and the cross-attention mask (xa_mask) to the cross-attention layer.
         # Don't forget to add the residual connections after each layer, and
         # to apply the normalizations on the inputs of each layer.
-        ??? # TODO
+        # TODO
+        residual = x
+        x = self.norm1(x)
+        x = self.self_attn(x, sa_mask)
+        x_a = x +  residual
 
+        residual = x_a
+        x = self.query_norm(x_a)
+        context = self.context_norm(context)
+        x = self.cross_attn(x, context, xa_mask)
+        x_b = x + residual
+
+        residual = x_b
+        x = self.norm2(x_b)
+        x = self.mlp(x)
+        x_c =  x + residual
+
+        return x_c
+        
+        
 class TransformerTrunk(nn.Module):
     """Basic Transformer trunk definition that can be used for encoder-only,
     decoder-only and prefixLM models, depending on the attention mask applied.
@@ -311,7 +336,14 @@ class TransformerDecoderTrunk(nn.Module):
         ):
         super().__init__()
 
-        self.blocks = ??? # TODO: Create a list of transformer decoder blocks and wrap inside nn.ModuleList
+        self.blocks = nn.ModuleList([
+            DecoderBlock(
+                dim=dim, 
+                head_dim=head_dim, 
+                mlp_ratio=mlp_ratio, 
+                use_bias=use_bias
+            ) for _ in range(depth)
+        ]) # TODO: Create a list of transformer decoder blocks and wrap inside nn.ModuleList
     
     def forward(
             self, 
@@ -321,4 +353,7 @@ class TransformerDecoderTrunk(nn.Module):
             xa_mask: Optional[torch.Tensor] = None, # Cross-attention mask
         ) -> torch.Tensor:
         
-        ??? # TODO
+        for block in self.blocks:
+            x = block(x, context, sa_mask=sa_mask, xa_mask=xa_mask)
+        
+        return x
